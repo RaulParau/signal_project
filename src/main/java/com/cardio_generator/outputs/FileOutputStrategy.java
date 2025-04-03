7fbb424c99097f46e79e223886179debc2d4990d
Raul <r.parau@student.maastrichtuniversity.nl>
2025-04-03 10:23:38 +0200

    Corrected the file name

--- src/main/java/com/cardio_generator/outputs/FileOutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/FileOutputStrategy.java
@@ -6,7 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.concurrent.ConcurrentHashMap;
-
+// THE FILE NAME WAS WRONG
 public class FileOutputStrategy implements OutputStrategy {
 
     private String BaseDirectory;
