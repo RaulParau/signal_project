dfb7bf835e7b4cb7d4c4bd69e1d20139b962cff2
Raul <r.parau@student.maastrichtuniversity.nl>
2025-04-04 12:20:14 +0200

    Added Javadoc Documentation to interface PatientDataGenerator

--- src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
+++ src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
@@ -1,7 +1,17 @@
 package com.cardio_generator.generators;
 
 import com.cardio_generator.outputs.OutputStrategy;
-
+/**
+ * This Interface provides a contract to implement the method "generate"
+ *
+ * @author RaulParau
+ */
 public interface PatientDataGenerator {
+    /**
+     * Generates data for a specific patient and outputs the data in a specified way
+     *
+     * @param patientId The patient ID of the patient the data is generated for
+     * @param outputStrategy Specifies the strategy of outputting the data
+     */
     void generate(int patientId, OutputStrategy outputStrategy);
 }
