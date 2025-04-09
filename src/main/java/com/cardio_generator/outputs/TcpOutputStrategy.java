b9d22ad713769141c7002fb8204e8a3b2e367797
Raul <r.parau@student.maastrichtuniversity.nl>
2025-04-09 17:50:25 +0200

    Finished documentation of TcpOutputStrategy class

--- src/main/java/com/cardio_generator/outputs/TcpOutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/TcpOutputStrategy.java
@@ -6,12 +6,32 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.Executors;
 
+/**
+ * This class implements the OutputStrategy interface and is responsible for sending ouptut data to clients
+ *  over a TCP connection. It does this by listening to a specified port for client connections and sends data
+ *  back to that client
+ */
 public class TcpOutputStrategy implements OutputStrategy {
 
+    /**
+     * Server socket that listens for client connection.
+     */
     private ServerSocket serverSocket;
+
+    /**
+     * Server socket of the client.
+     */
     private Socket clientSocket;
+
+    /**
+     * Writer that is used to send data to the client.
+     */
     private PrintWriter out;
 
+    /**
+     * Creates a TcpOutputStrategy object that starts the TCP server on a specified port
+     * @param port port on that the TCP server is started
+     */
     public TcpOutputStrategy(int port) {
         try {
             serverSocket = new ServerSocket(port);
@@ -32,6 +52,15 @@ public class TcpOutputStrategy implements OutputStrategy {
         }
     }
 
+    /**
+     * Sends data to the patient that is connected to the server in a comma seperated form.
+     * The message that is sent back contains patientId, the timestamp, the label and the data.
+     *
+     * @param patientId The patients patient ID
+     * @param timestamp Moment in time when the data was generated
+     * @param label A label of the data, describing the type of data
+     * @param data The generated data
+     */
     @Override
     public void output(int patientId, long timestamp, String label, String data) {
         if (out != null) {
