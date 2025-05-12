f90ccacafb3be20015c2bcd48bf5f0e654f32a4d
Raul <r.parau@student.maastrichtuniversity.nl>
2025-05-12 10:45:37 +0200

    Implemented the JUnit test for the SelfTriggeredAlert.

---
+++ src/test/java/data_management/SelfTriggeredAlertTest.java
@@ -0,0 +1,35 @@
+package data_management;
+
+import com.alerts.Alert;
+import com.alerts.SelfTriggeredAlert;
+import com.data_management.PatientRecord;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.alerts.AlertType.MANUAL_TRIGGER;
+
+public class SelfTriggeredAlertTest {
+    private SelfTriggeredAlert alertCondition;
+
+    @BeforeEach
+    public void setUp(){
+        alertCondition = new SelfTriggeredAlert();
+    }
+
+    @Test
+    public void testForSelfTriggeredAlert(){
+        List<PatientRecord> records = new ArrayList<>();
+
+        records.add(new PatientRecord(1, 1.0, "Button was pressed", 1));
+
+        Alert alert = alertCondition.checkCondition(records);
+
+        Assertions.assertNotNull(alert);
+        Assertions.assertEquals(MANUAL_TRIGGER, alert.getAlertType());
+        Assertions.assertEquals("Manual Triggered Alert: Nurse/Patient button pressed", alert.getCondition());
+    }
+}
