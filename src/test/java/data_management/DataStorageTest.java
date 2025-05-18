fda826e3ec67206c77c26e45ec679ad5feae5614
Raul <r.parau@student.maastrichtuniversity.nl>
2025-05-18 17:15:12 +0200

    I made the JUnit tests for the DataStorage class

--- src/test/java/data_management/DataStorageTest.java
+++ src/test/java/data_management/DataStorageTest.java
@@ -2,13 +2,11 @@ package data_management;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.data_management.DataReader;
-import com.data_management.ReadData;
+import com.data_management.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import com.data_management.DataStorage;
-import com.data_management.PatientRecord;
-
+import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.util.List;
 
@@ -16,7 +14,6 @@ class DataStorageTest {
     @Test
     void testAddAndGetRecords() {
         // TODO Perhaps you can implement a mock data reader to mock the test data?
-        DataReader reader;
         DataStorage storage = new DataStorage();
 
         storage.addPatientData(1, 100.0, "WhiteBloodCells", 1714376789050L);
@@ -27,6 +24,35 @@ class DataStorageTest {
         assertEquals(100.0, records.get(0).getMeasurementValue()); // Validate first record
     }
 
+    @Test
+    void testGetAllPatient(){
+        DataStorage storage = new DataStorage();
 
+        storage.addPatientData(1, 99, "Saturation", 1000L);
+        storage.addPatientData(2, 120, "SystolicPressure", 2000L);
 
+        List <Patient> patients = storage.getAllPatients();
+        assertEquals(2, patients.size());
+    }
+
+    @Test
+    void testGetAllRecordsInTimeframe(){
+        DataStorage storage = new DataStorage();
+
+        storage.addPatientData(1, 99, "Saturation", 1000L);
+        storage.addPatientData(1, 120, "SystolicPressure", 4000L);
+
+        List<PatientRecord> records = storage.getRecords(1, 2000L, 5000L);
+
+        assertEquals(1, records.size());
+        assertEquals(120, records.get(0).getMeasurementValue());
+
+    }
+
+    @Test
+    void testReturnsNoRecordForUnknownPatient(){
+        DataStorage storage = new DataStorage();
+        List<PatientRecord> records =  storage.getRecords(90, 0, 100L);
+        assertTrue(records.isEmpty());
+    }
 }
